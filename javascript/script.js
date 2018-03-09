362436a5cc259cadb98d108e50b13fe3fd332dc9
Cliff Chan <cchan17@live.ca>
2018-03-09 13:27:59 -0800

    changes for project 6

--- javascript/script.js
+++ javascript/script.js
@@ -9,30 +9,29 @@ $('.main-carousel').flickity({
   prevNextButtons: false
 });
 
-$("form").on("submit", function() {
+$("form").on("submit", function () {
   var emailValue;
   emailValue = $("input").val();
- 
-  if($("#email-form").val()=="") {
+
+  if ($("#email-form").val() == "") {
     alert("Please input a valid email.");
     return false;
   } else {
-    alert ("Thanks for subscribing!");
+    alert("Thanks for subscribing!");
     return false;
- 
+
   }
-  });
+});
 
 // smoothscroll
 $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
-  .click(function(event) {
+  .click(function (event) {
     // On-page links
     if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
       location.hostname == this.hostname
     ) {
       // Figure out element to scroll to
@@ -44,7 +43,7 @@ $('a[href*="#"]')
         event.preventDefault();
         $('html, body').animate({
           scrollTop: target.offset().top
-        }, 1000, function() {
+        }, 1000, function () {
           // Callback after animation
           // Must change focus!
           var $target = $(target);
@@ -52,7 +51,7 @@ $('a[href*="#"]')
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
           };
         });
